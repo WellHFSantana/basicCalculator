5863e27a78625aa49b593e5289ee3555d9563932
Wellington Henrique França de Santana <53205033+WellHFSantana@users.noreply.github.com>
2022-08-09 22:30:19 -0300

    Add files via upload

--- calculatorCorretion.py
+++ calculatorCorretion.py
@@ -7,27 +7,27 @@ Please type in the math operation you would like to complete:
 / for division
 ''')
 
-    number_1 = int(input('Please enter the first number: '))
-    number_2 = int(input('Please enter the second number: '))
+    num_1 = int(input('Please enter the first number: '))
+    num_2 = int(input('Please enter the second number: '))
 
     if operation == '+':
-        print('{} + {} = '.format(number_1, number_2))
-        print(number_1 + number_2)
+        print('{} + {} = '.format(num_1, num_2))
+        print(num_1 + num_2)
 
     elif operation == '-':
-        print('{} - {} = '.format(number_1, number_2))
-        print(number_1 - number_2)
+        print('{} - {} = '.format(num_1, num_2))
+        print(num_1 - num_2)
 
     elif operation == '*':
-        print('{} * {} = '.format(number_1, number_2))
-        print(number_1 * number_2)
+        print('{} * {} = '.format(num_1, num_2))
+        print(num_1 * num_2)
 
     elif operation == '/':
-        print('{} / {} = '.format(number_1, number_2))
-        print(number_1 / number_2)
+        print('{} / {} = '.format(num_1, num_2))
+        print(num_1 / num_2)
 
     else:
-        print('You have not typed a valid operator, please run the program again.')
+        print('Sorry,you havent typed a valid operator, please run the program again.')
 
     # Add again() function to calculate() function
     again()
